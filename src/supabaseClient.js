253b736a4aaad76286e29f5b58d07dbd6dd6ce93
xxzarkx <rodrigofranco.1301@gmail.com>
2025-01-24 08:04:48 -0500

    agregando .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://hfroriigyffeomzxxlnw.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmcm9yaWlneWZmZW9tenh4bG53Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzE5MzM3MiwiZXhwIjoyMDQ4NzY5MzcyfQ.hGoIHaGaLyZIB5pTnmPBXdxMOTpbx0m0Z3o5IFoHfSQ";
-
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
